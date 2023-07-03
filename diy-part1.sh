85b82630810d029bd45856a15926e3f858089170
Mrzqd <104408988+Mrzqd@users.noreply.github.com>
2023-07-03 17:37:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,8 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+wget https://github.com/vernesong/OpenClash/archive/master.zip
+## 解压
+unzip master.zip
+## 复制OpenClash软件包到OpenWrt
+cp -r OpenClash-master/luci-app-openclash package/
